kbarbel640-del/openclaw

feat(config): auto-rollback to last known-good backup on invalid config startup (#18200)
When the gateway detects an invalid `openclaw.json` at startup, instead
of crashing immediately it now iterates through existing `.bak` backups
and restores the first one that passes validation.  The broken config is
preserved as `openclaw.json.broken-<timestamp>` for post-mortem debugging.

If no valid backup is found the gateway still exits with a clear error,
matching the previous behaviour.

Co-authored-by: Cursor <cursoragent@cursor.com>

src/config/io.ts
@@ -1,9 +1,10 @@
+import JSON5 from "json5";
 import crypto from "node:crypto";
 import fs from "node:fs";
 import os from "node:os";
 import path from "node:path";
 import { isDeepStrictEqual } from "node:util";
-import JSON5 from "json5";
+import type { OpenClawConfig, ConfigFileSnapshot, LegacyConfigIssue } from "./types.js";
 import { loadDotEnv } from "../infra/dotenv.js";
 import { resolveRequiredHomeDir } from "../infra/home-dir.js";
 import {
@@ -14,7 +15,7 @@ import {
 } from "../infra/shell-env.js";
 import { VERSION } from "../version.js";
 import { DuplicateAgentDirError, findDuplicateAgentDirs } from "./agent-dirs.js";
-import { rotateConfigBackups } from "./backup-rotation.js";
+import { CONFIG_BACKUP_COUNT, rotateConfigBackups } from "./backup-rotation.js";
 import {
   applyCompactionDefaults,
   applyContextPruningDefaults,
@@ -38,7 +39,6 @@ import { applyMergePatch } from "./merge-patch.js";
 import { normalizeConfigPaths } from "./normalize-paths.js";
 import { resolveConfigPath, resolveDefaultConfigCandidates, resolveStateDir } from "./paths.js";
 import { applyConfigOverrides } from "./runtime-overrides.js";
-import type { OpenClawConfig, ConfigFileSnapshot, LegacyConfigIssue } from "./types.js";
 import {
   validateConfigObjectRawWithPlugins,
   validateConfigObjectWithPlugins,
@@ -1130,4 +1130,53 @@ export async function writeConfigFile(
   await io.writeConfigFile(cfg, {
     envSnapshotForRestore: sameConfigPath ? options.envSnapshotForRestore : undefined,
   });
+}
+
+/**
+ * Iterate through config backup files (.bak, .bak.1, …) and return the first
+ * snapshot that passes validation.  Returns `null` when no usable backup exists.
+ */
+export async function tryLoadValidConfigBackup(
+  configPath: string,
+): Promise<{ snapshot: ConfigFileSnapshot; backupPath: string } | null> {
+  const backupBase = `${configPath}.bak`;
+  const candidates = [backupBase];
+  for (let i = 1; i < CONFIG_BACKUP_COUNT; i++) {
+    candidates.push(`${backupBase}.${i}`);
+  }
+
+  for (const candidate of candidates) {
+    try {
+      const raw = await fs.promises.readFile(candidate, "utf-8");
+      const parsed = JSON5.parse(raw);
+      if (typeof parsed !== "object" || parsed === null) {
+        continue;
+      }
+
+      const validated = validateConfigObjectRawWithPlugins(parsed);
+      if (!validated.ok) {
+        continue;
+      }
+
+      return {
+        snapshot: {
+          path: candidate,
+          exists: true,
+          raw,
+          parsed,
+          resolved: parsed as OpenClawConfig,
+          valid: true,
+          config: parsed as OpenClawConfig,
+          issues: [],
+          warnings: validated.warnings ?? [],
+          legacyIssues: [],
+        },
+        backupPath: candidate,
+      };
+    } catch {
+      // Backup missing or unreadable — try the next one.
+    }
+  }
+
+  return null;
 }

dadasersin/openclaw-yapay-zeka-asistan-

src/config/io.ts
@@ -1,10 +1,11 @@
+import JSON5 from "json5";
+import { ensureOwnerDisplaySecret } from "../agents/owner-display.js";
+import type { OpenClawConfig, ConfigFileSnapshot, LegacyConfigIssue } from "./types.js";
 import crypto from "node:crypto";
 import fs from "node:fs";
 import os from "node:os";
 import path from "node:path";
 import { isDeepStrictEqual } from "node:util";
-import JSON5 from "json5";
-import { ensureOwnerDisplaySecret } from "../agents/owner-display.js";
 import { loadDotEnv } from "../infra/dotenv.js";
 import { resolveRequiredHomeDir } from "../infra/home-dir.js";
 import {
@@ -16,7 +17,7 @@ import {
 import { sanitizeTerminalText } from "../terminal/safe-text.js";
 import { VERSION } from "../version.js";
 import { DuplicateAgentDirError, findDuplicateAgentDirs } from "./agent-dirs.js";
-import { maintainConfigBackups } from "./backup-rotation.js";
+import { CONFIG_BACKUP_COUNT, maintainConfigBackups } from "./backup-rotation.js";
 import {
   applyCompactionDefaults,
   applyContextPruningDefaults,
@@ -48,7 +49,6 @@ import { normalizeConfigPaths } from "./normalize-paths.js";
 import { resolveConfigPath, resolveDefaultConfigCandidates, resolveStateDir } from "./paths.js";
 import { isBlockedObjectKey } from "./prototype-keys.js";
 import { applyConfigOverrides } from "./runtime-overrides.js";
-import type { OpenClawConfig, ConfigFileSnapshot, LegacyConfigIssue } from "./types.js";
 import {
   validateConfigObjectRawWithPlugins,
   validateConfigObjectWithPlugins,
@@ -1412,4 +1412,53 @@ export async function writeConfigFile(
     envSnapshotForRestore: sameConfigPath ? options.envSnapshotForRestore : undefined,
     unsetPaths: options.unsetPaths,
   });
+}
+
+/**
+ * Iterate through config backup files (.bak, .bak.1, …) and return the first
+ * snapshot that passes validation.  Returns `null` when no usable backup exists.
+ */
+export async function tryLoadValidConfigBackup(
+  configPath: string,
+): Promise<{ snapshot: ConfigFileSnapshot; backupPath: string } | null> {
+  const backupBase = `${configPath}.bak`;
+  const candidates = [backupBase];
+  for (let i = 1; i < CONFIG_BACKUP_COUNT; i++) {
+    candidates.push(`${backupBase}.${i}`);
+  }
+
+  for (const candidate of candidates) {
+    try {
+      const raw = await fs.promises.readFile(candidate, "utf-8");
+      const parsed = JSON5.parse(raw);
+      if (typeof parsed !== "object" || parsed === null) {
+        continue;
+      }
+
+      const validated = validateConfigObjectRawWithPlugins(parsed);
+      if (!validated.ok) {
+        continue;
+      }
+
+      return {
+        snapshot: {
+          path: candidate,
+          exists: true,
+          raw,
+          parsed,
+          resolved: parsed as OpenClawConfig,
+          valid: true,
+          config: parsed as OpenClawConfig,
+          issues: [],
+          warnings: validated.warnings ?? [],
+          legacyIssues: [],
+        },
+        backupPath: candidate,
+      };
+    } catch {
+      // Backup missing or unreadable — try the next one.
+    }
+  }
+
+  return null;
 }