kbarbel640-del/openclaw

fix(auth): ownerAllowFrom wildcard now grants senderIsOwner
When commands.ownerAllowFrom is set to ["*"], senderIsOwner was always
false because ownerAllowAll produced an empty ownerList, making
matchedSender undefined. This caused owner-only tools (cron, gateway)
to be filtered from the agent's tool list.

Now ownerAllowAll feeds into senderIsOwner, consistent with how it
already feeds into isOwnerForCommands.

Closes openclaw/openclaw#25286

✍️ Author: Claude Code with @carrotRakko (AI-written, human-approved)

src/auto-reply/command-auth.ts
@@ -341,7 +341,7 @@ export function resolveCommandAuthorization(params: {
   const senderId = matchedSender ?? senderCandidates[0];
 
   const enforceOwner = Boolean(dock?.commands?.enforceOwnerForCommands);
-  const senderIsOwner = Boolean(matchedSender);
+  const senderIsOwner = ownerAllowAll || Boolean(matchedSender);
   const ownerAllowlistConfigured = ownerAllowAll || explicitOwners.length > 0;
   const requireOwner = enforceOwner || ownerAllowlistConfigured;
   const isOwnerForCommands = !requireOwner

dadasersin/openclaw-yapay-zeka-asistan-

src/auto-reply/command-auth.ts
@@ -346,7 +346,7 @@ export function resolveCommandAuthorization(params: {
   const senderId = matchedSender ?? senderCandidates[0];
 
   const enforceOwner = Boolean(dock?.commands?.enforceOwnerForCommands);
-  const senderIsOwnerByIdentity = Boolean(matchedSender);
+  const senderIsOwnerByIdentity = ownerAllowAll || Boolean(matchedSender);
   const senderIsOwnerByScope =
     isInternalMessageChannel(ctx.Provider) &&
     Array.isArray(ctx.GatewayClientScopes) &&